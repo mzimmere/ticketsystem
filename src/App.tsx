205274743a1327c5943b8fff6643f8c5a499a534
Matthias <Matthias.zimmerer@outlook.com>
2026-07-02 08:31:07 +0200

    Volltextsuche aktiviert, Hero-Bild resized statt abschneidet

--- src/App.tsx
+++ src/App.tsx
@@ -591,7 +591,7 @@ export default function App() {
                     <img
                       src={organisation.hero_bild_url}
                       alt=""
-                      className="h-40 w-full object-cover sm:h-56"
+                      className="h-40 w-full object-contain sm:h-56"
                     />
                   )}
                   {organisation?.motto && (
